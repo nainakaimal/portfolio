23353831eabc9e7c0ff1f5d6909adca6ea11f1f8
nainakaimal <naina.kaimal@gmail.com>
2017-07-06 11:53:15 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
+document.getElementById("shapeimg");
 
+onmouseover="mOver(this)" onmouseout="mOut(this)" 
+
+
+function mOver(obj) {
+    obj.innerHTML = img src="Shapebw.png
+}
+
+function mOut(obj) {
+    obj.innerHTML = img src="Shape.png"
+}
